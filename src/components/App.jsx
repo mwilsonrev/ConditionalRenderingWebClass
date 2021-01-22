90230f18700c579413bca3720a2177c278a9ce3a
Mark Wilson <mwilsonrev@gmail.com>
2021-01-22 09:38:39 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Login from "./loginForm";
+
+var isLoggedIn = true;
+
+function renderConditionally() {
+  if (isLoggedIn === true) {
+    return <h1>Hello</h1>;
+  } else {
+    return <Login />;
+  }
+}
+
+function App() {
+  return <div className="container">{renderConditionally()}</div>;
+}
+
+export default App;
